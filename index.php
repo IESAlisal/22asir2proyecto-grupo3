5a0326a354e25eb089716fa720ceaa4ec2d05dce
AguGomezPaneda <97469757+AguGomezPaneda@users.noreply.github.com>
2022-12-20 19:57:14 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,38 +5,28 @@ if(!isset($_SESSION["usuario"]) || $_SESSION["usuario"]==null){
     header("Location: index.php");
 }
 
-?><html>
+?>
+<html>
     <head>
         <title>Aplicacion de Gestion de libros</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            body{
-                text-align:center;
-            }
-            #menu{
-                display:inline-block;
-                text-align:left;
-            }
-            #menu li{
-                margin-top:2em;
-            }
-        </style>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/estiloIndex.css">
     </head>
     <body>
-    
-        <h1>Bienvenido a la aplicacion de libros</h1>
         <div id="menu">
             <ul>
+                <h2>Aplicacion de libros</h2>
                 <li><a href="libros_MySQLi.php">Alta Libros</a></li>
-                <li><a href="libros_actualizar.php">Acualizar Libros</a></li>
+                <li><a href="libros_actualizar.php">Actualizar Libros</a></li>
                 <li><a href="libros_borrar_MySQLi.php">Baja Libros</a></li>
+                <form action='logoff.php' method='post'>
+                                <input type='submit' name='desconectar' class="btn btn-warning" value='Desconectar usuario <?php echo
+                            $_SESSION['usuario'];
+                            ?>'/>
+                </form>
             </ul>
         </div>
-        <form action='logoff.php' method='post'>
-	                    <input type='submit' name='desconectar' class="btn btn-warning" value='Desconectar usuario <?php echo
-	                $_SESSION['usuario'];
-	                ?>'/>
-	    </form>
     </body>
 </html>
