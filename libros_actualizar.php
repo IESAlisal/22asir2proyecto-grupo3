8715a381f9e8b7c31bbbc133aff86c64bd6c207d
AguGomezPaneda <97469757+AguGomezPaneda@users.noreply.github.com>
2022-12-21 20:10:04 +0100

    Add files via upload

--- libros_actualizar.php
+++ libros_actualizar.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" media="screen" href="css/estiloActualizar.css" >
 </head>
 <body>
+
 	<?php
 	   ini_set("display_errors",true);
 		require_once 'funcionesBaseDatos.php';
@@ -17,64 +18,68 @@
 		else{
 		}
 	?>
-		<form class="formulario" action="" method="post" name="formulario">
-			<ul>
-				<li>
-					<h2>Libros que se van a actualizar</h2>
-					<label for="libro">Libros:*</label>
-					<select name="libro">
-						<?php
-							$libros = getLibrosTitulo();
-							foreach ($libros as $libro) 
-							{
-								echo "<option value='$libro'";
-								if (isset($_POST['libro']) && $libro == $_POST['libro'])
-									echo " selected='true'";
 
-								echo ">$libro</option>";
-							}
-						?>
-					</select>
-				</br>
-					<button class="submit" type="submit" name="mostrar">Mostrar</button>
-					</li>
-			</ul>
-		</form>
+	<form class="formulario" action="" method="post" name="formulario">
+	    <ul>
+		    <li>
+		         <h2>Libros que se van a actualizar</h2>
+				<label for="libro">Libros:*</label>
+		        <select name="libro">
+		            <?php
+						$libros = getLibrosTitulo();
+						foreach ($libros as $libro) 
+						{
+						    echo "<option value='$libro'";
+						    if (isset($_POST['libro']) && $libro == $_POST['libro'])
+                        	    echo " selected='true'";
+
+						    echo ">$libro</option>";
+						}
+		    		?>
+		        </select>
+			</br>
+				<button class="submit" type="submit" name="mostrar">Mostrar</button>
+				</li>
+		</ul>
+	</form>
+		<?php
+		    if (isset($_POST['mostrar'])) 
+		    {
+		?>
+		<form id="actualizarPrecio" method="post" action="">
+		<table class="tabla">
+
+		<tbody>
+			<tr>
+				<th>Titulo</th>
+				<th>Precio</th>
+			</tr>
+			<tr>
 			<?php
-				if (isset($_POST['mostrar'])) 
-				{
+		        
+		        $libro = $_POST['libro'];
+		        $librosprecios = getLibrosPrecio($libro);
+		        foreach ($librosprecios as $libroprecio) 
+		        {
+		        	echo "<input type='hidden' name='libro' value='{$_POST['libro']}'>"
+		        	echo "<tr>"."<input type='hidden' name='librosprecios[]' value='{$libroprecio['numero_ejemplar']}'>";
+		        	echo "<td>".$libroprecio["titulo"]."</td>";
+		        	echo "<td><input type='text' size='4' name='precio[]' value='{$libroprecio['precio']}'> Euros </td></tr>";
+		        }
 			?>
-			<form id="actualizarPrecio" method="post" action="">
-			<table class="tabla">
-
-			<tbody>
-				<tr>
-					<th>Titulo</th>
-					<th>Precio</th>
-				</tr>
-				<tr>
-				<?php
-					
-					$libro = $_POST['libro'];
-					$librosprecios = getLibrosPrecio($libro);
-					foreach ($librosprecios as $libroprecio) 
-					{
-						echo "<input type='hidden' name='libro' value='{$_POST['libro']}'>"
-						echo "<tr>"."<input type='hidden' name='librosprecios[]' value='{$libroprecio['numero_ejemplar']}'>";
-						echo "<td>".$libroprecio["titulo"]."</td>";
-						echo "<td><input type='text' size='4' name='precio[]' value='{$libroprecio['precio']}'> Euros </td></tr>";
-					}
-				?>
-				</tr>
-			</tbody>
-		</table>
-			<button class="submit actualizar" type="submit" name="actualizar">Actualizar</button>
-		</br>
-			<?php
-				}
-			?>		
-			<br>
-		<a href="index.php">Volver</a>
-		</form>
-	</body>
+			</tr>
+		</tbody>
+	</table>
+		<button class="submit actualizar" type="submit" name="actualizar">Actualizar</button>
+</br>
+		<?php
+		    }
+		?>		
+		<br>
+	<a href="index.php">Volver</a>
+	</form>
+	
+		
+	
+</body>
 </html>
