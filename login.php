57aafcf56d0a212bfd4ac05ac5cc1e26df3d1345
AguGomezPaneda <97469757+AguGomezPaneda@users.noreply.github.com>
2022-12-22 01:46:02 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -17,35 +17,38 @@ if (isset($_POST['login'])) {
         }
         else
         {
-            $error = "¡Usuario o contraseña no válidos!";
+            
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Grupo3</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="css/estiloLogin.css">
+  </head>
+  <body>
+    
+  <?php if(isset($error)) echo "<div class='error'>$error</div>"; ?>
+  
+    <form action="form-signin" class="form-box" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>' method='post'>">
+      <h1 class="form-title">Login</h1>
+      <input type="text" name="usuario" class="form-control" placeholder="Usuario" required autofocus>
+      <input type="password" name="password" class="form-control" placeholder="Contraseña" required>
+      <button type="submit" name="login">
+        Iniciar Sesion
+      </button>
+      $error = "¡Usuario o contraseña no válidos!";
         }
     }
 }
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/estiloLogin.css">
-  </head>
-
-  <body class="text-center">
-    
-  <?php if(isset($error)) echo "<div class='error'>$error</div>"; ?>
-  
-    <h2 class="h3 mb-3 font-weight-normal">Login</h2>
-    <form class="form-signin" action='<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>' method='post'>
-      <label for="usuario" class="sr-only">Usuario</label>
-      <input type="text" name="usuario" class="form-control" placeholder="Usuario" required autofocus>
-      <label for="password" class="sr-only">Contraseña</label>
-      <input type="password" name="password" class="form-control" placeholder="Contraseña" required>
-      </br>
-      <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Iniciar Sesion</button> 
       <p>¿Aún no tienes cuenta?</p>
-      <a class="btn btn-primary" href="registro.php">Registrate aqui</a>
+      <a class="form-register" href="registro.php">Registrate aqui</a> 
     </form>
+
   </body>
 </html>
